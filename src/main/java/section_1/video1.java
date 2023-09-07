83749f42d96bf6510035090edbf0f9a220a9d22c
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-09-07 18:09:55 +0530

    playWright

--- src/main/java/section_1/video1.java
+++ src/main/java/section_1/video1.java
@@ -6,5 +6,15 @@ public class video1 {
      * So you do not need to put any hardCode sleep and all in the playWright.
      * -----------------------------------------------------------------------
      * playWright have some inbuilt feature to handle the autoWait.
+     *
+     * This link "https://playwright.dev/docs/actionability" is helpful.
+     * Go through the document where you can understand the click event, how it will happen in playWright.
+     * So Before to click happen it will check all the states which mentioned in document.
+     * Like : all states means -> "Attached","Visible","Stable","Receives Events","Enabled","Editable"
+     * Note : PLAYWRIGHT will CHECK all the state then after it will continue.
+     * ======================================================================================================
+     * so in other automation tools the moment appears it will try to click on it, may be by that time it is not
+     * in a stable mode or fully visible then you will run into errors.
+     * But in PlayWright first it will CHECK all the state then after it will continue.
      */
 }

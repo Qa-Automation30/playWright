d62e72617e4e9b02117d151f81e6434c3d33d841
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-09-07 17:59:24 +0530

    playWright

---
+++ src/main/java/section_1/video1.java
@@ -0,0 +1,10 @@
+package section_1;
+
+public class video1 {
+    /**
+     * 1 -> Playwright handles the modern webApps with Auto-wait capability
+     * So you do not need to put any hardCode sleep and all in the playWright.
+     * -----------------------------------------------------------------------
+     * playWright have some inbuilt feature to handle the autoWait.
+     */
+}

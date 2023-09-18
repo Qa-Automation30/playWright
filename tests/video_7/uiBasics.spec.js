8c5a37335dc2c87e402fc750e7e0e2a978f1246e
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-09-18 18:56:19 +0530

    changes has been done

---
+++ tests/video_7/uiBasics.spec.js
@@ -0,0 +1,8 @@
+/**
+ *   The imp point is here, from where you need to run your test cases -> So you can run your test cases from 
+ *   "playWright.config.js" file. All the configurations are declared here.
+ */
+/**
+ *  Second point is that you need to write your test cases inside the tests folder. This is some Kind of rule
+ */
+const {test} = require('@playwright/test');

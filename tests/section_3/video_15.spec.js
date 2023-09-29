cdc3d664cb683687c2ea5ab35cac3e5129d0e99a
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-09-29 13:27:43 +0530

    added

--- tests/section_3/video_15.spec.js
+++ tests/section_3/video_15.spec.js
@@ -1,4 +1,5 @@
 // This is very very IMP VIDEO, need to watch again and again
+// need to understand the concept again here
 
 const { test, expect } = require('@playwright/test')
 
@@ -6,14 +7,13 @@ test("Auto wait fun test", async function ({ browser }) {
     const context = await browser.newContext();
     const page = await context.newPage();
     // Navigate to this URL
-    await page.goto("https://rahulshettyacademy.com/client/auth/login");
+    await page.goto("https://rahulshettyacademy.com/client");
     // enter the correct userName and password and click on signIn button
     await page.locator("#userEmail").type("anshika@gmail.com")
     await page.locator("#userPassword").type("Iamking@000")
     await page.locator("[type='submit']").click();
-    // this is the concept which is already discussed in previous video
-    await page.locator(".card-body b").first().textContent();
+    // this below line is very very imp and need to underStand the concept of newtwork calls
+    await page.waitForLoadState("networkidle");
     const allTitles = await page.locator(".card-body b").allTextContents();
-
     console.log(allTitles);
 });

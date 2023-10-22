07deb9fa33962943c82a3e1f4521de04fdc8ec50
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-10-22 11:14:31 +0530

    added more learning3

---
+++ tests/Pawan-tutorials-Youtube/Video_33_Annotaions/anootations.spec.js
@@ -0,0 +1,20 @@
+import {test,expect} from '@playwright/test'
+
+    test('Test case -1', async () => {
+        console.log("This is the test-1 and belongs to sanity");
+    });
+    //==========================================
+    test('Test case -2', async () => {
+        console.log("This is the test-2");
+    });
+
+
+test('Test case -3', async () => {
+    console.log("This is the test-3");
+});
+//==========================================
+test('Test case -4', async () => {
+    console.log("This is the test-4");
+});
+
+//npx playwright test tags --grep "@sanity"

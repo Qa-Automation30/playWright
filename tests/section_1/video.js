86bf06f38c335e8f457889ea53bbbec120615dff
bitbucket_29 <vivek.srivastavacs65@gmail.com>
2023-12-02 10:34:23 +0530

    revision

--- tests/section_1/video.js
+++ tests/section_1/video.js
@@ -1,11 +1,11 @@
 /**
- * This section is all about how to setup the env fro playWright and run the testCases.
+ * This section is all about how to setup the env for playWright and run the testCases.
  */
 /**
      * 1-> So we are using "javascript" code for writing our test case, so that is why we need to have a platform to run JS code.
-     * so for JS code "node.js" platform engine is required, which supports JS related code.
+     * so for JS code-> "node.js" platform engine is required, which supports JS related code.
      * ==========================================================================================
-     * so first you need to install the node js from the website for winodws application. >> adterdownload you need to install the nodeSoftware.
+     * so first you need to install the "node js" from the website for winodws application. >> afterdownload you need to install the nodeSoftware.
      * as soon as you install, you need to set the path in system-variables-> as NODE_HOME-> c>> programeFiles >> nodeJS [copy the path from here]
      * Now you need one editior , you can pick visual studio, best for JS.
      * 
